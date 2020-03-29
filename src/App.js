51e629d061de1ffff0b8f0c53b5b6a677a0ae253
aayushi kambariya <aayushi.kambriya5@gmail.com>
2020-03-29 21:19:24 +0530

    using react Hooks in functional components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = props => {
 
-  state = {
-    person: [
-      {name:'Aayushi', age:24, female:true },
-      {name:'Allan', age:22, female:false},
-      {name:'Anahita', age:25, female:true},
-    ]
-  }
+  const [ personState , setPersonState ] = useState({
+      person: [
+        { name:'Aayushi', age:24, female:true },
+        { name:'Allan', age:22, female:false },
+        { name:'Anahita', age:25, female:true },
+        
+      ],
+      otherState : 'some other value',
+    }
+  )
 
-  switchNameHandler = () => {
-    this.setState( {
+  const [useOtherState, seOtherState] = useState({
+    otherState: 'other some value'
+  })
+  
+  console.log(personState,useOtherState);
+
+  const switchNameHandler = () => {
+    setPersonState( {
         person : [
           {name:'Monikesh', age:30, female:false },
-          {name:'Namra', age:24, female:true},
-          {name:'Brandon', age:26, female:false},
-        ]
+          {name:'Nomi', age:24, female:true},
+          {name:'Baller', age:26, female:false},
+        ],
+
       }
     )
   }
+
   
-  render() {
+  // render() {
     return (
       <div className="App">
        <h1>Hello, This is a react app.</h1>
-       <button onClick={this.switchNameHandler}>Click me</button>
-       <Person name={this.state.person[0].name} age={this.state.person[0].age} gender={this.state.person[0].female ? 'she' : 'he'}/>
-       <Person name={this.state.person[1].name} age={this.state.person[1].age} gender={this.state.person[1].female ? 'she' : 'he'}/>
-       <Person name={this.state.person[2].name} age={this.state.person[2].age} gender={this.state.person[2].female ? 'she' : 'he'}/>
+       <button onClick={switchNameHandler}>Click me</button>
+       <Person name={personState.person[0].name} age={personState.person[0].age} gender={personState.person[0].female ? 'she' : 'he'}/>
+       <Person name={personState.person[1].name} age={personState.person[1].age} gender={personState.person[1].female ? 'she' : 'he'}/>
+       <Person name={personState.person[2].name} age={personState.person[2].age} gender={personState.person[2].female ? 'she' : 'he'}/>
       </div>
     );
-  }
+  // }
 }
 
-export default App;
+export default app;
+
